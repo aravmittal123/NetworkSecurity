298f5f4b0b1c0176623a108203061b7d93910a21
Aarav Mittal <rekhahnd5731@gmail.com>
2026-08-20 19:17:31 +0530

    Data Ingestion completed

--- test_MongoDB.py
+++ test_MongoDB.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://rekhahnd5731_db_user:Admin123@cluster0.cnfiki4.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://rekhahnd5731_db_user:Lochan123@cluster0.cnfiki4.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
