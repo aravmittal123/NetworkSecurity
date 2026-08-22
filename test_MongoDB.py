0b003636830fc0025b6c922452226abb2298da69
Aarav Mittal <rekhahnd5731@gmail.com>
2026-08-22 18:23:43 +0530

    Data Validation completed

--- test_MongoDB.py
+++ test_MongoDB.py
@@ -1,14 +1,23 @@
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
+import pymongo
 
-uri = "mongodb+srv://rekhahnd5731_db_user:Lochan123@cluster0.cnfiki4.mongodb.net/?appName=Cluster0"
+MONGO_DB_URL = "mongodb+srv://rekhahnd5731_db_user:Lochan123@cluster0.cnfiki4.mongodb.net/?appName=Cluster0"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = pymongo.MongoClient(
+    MONGO_DB_URL,
+    serverSelectionTimeoutMS=10000
+)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+client.admin.command("ping")
+
+print("MongoDB connection successful!")
+
+print("\nDatabases:")
+for db_name in client.list_database_names():
+    print(" -", db_name)
+
+db = client["ARAVNYX"]
+
+print("\nCollections in ARAVNYX:")
+for collection_name in db.list_collection_names():
+    count = db[collection_name].count_documents({})
+    print(f" - {collection_name}: {count} documents")
